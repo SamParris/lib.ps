1d644f6b39d92fa49fb51b624693650a0957c5d9
SamParris <Sam.Parris@outlook.com>
2022-03-14 14:13:38 +0000

    Initial Commit

--- Profile/Profile.ps1
+++ Profile/Profile.ps1
@@ -17,22 +17,62 @@
 #Parameters
 $ErrorActionPreference = "Continue"
 
+#Region ---- Set Custom Prompt ----
+Function Global:Prompt {
+    [CmdletBinding()]
+    Param()
+    Process {
+        $LASTSUCCESS = $?
+        If ((New-Object Security.Principal.WindowsPrincipal(
+                    [Security.Principal.WindowsIdentity]::GetCurrent())
+            ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+            Write-Host "ADMIN::" -ForegroundColor Black -BackgroundColor Red -NoNewline
+        }
+        Write-Host "$('{0:d4}' -f $MyInvocation.HistoryId)>" -ForegroundColor Black -BackgroundColor Yellow -NoNewline
+        If ((Get-History).Count -ge 1) {
+            $CommandTimeSpan = (New-TimeSpan -Start (Get-History -Count 1).StartExecutionTime -End (Get-History -Count 1).EndExecutionTime).TotalMilliseconds
+            Write-Host ">$CommandTimeSpan>" -ForegroundColor Black -BackgroundColor DarkGray -NoNewline
+        }
+        If ((Get-Location -Stack).Count -ge 1) {
+            $StackLocation = (Get-Location -Stack).Count
+            Write-Host ">$StackLocation>" -ForegroundColor Black -BackgroundColor White -NoNewline
+        }
+        If ((Get-PSSession).Count -ge 1) {
+            $PSSessionCount = (Get-PSSession).Count
+            Write-Host ">$PSSessionCount>" -ForegroundColor Black -BackgroundColor Green -NoNewline
+        }
+        If ($DefaultVIServers) {
+            Write-Host ">$($DefaultVIServers.Name)>" -ForegroundColor Black -BackgroundColor Green -NoNewline
+        }
+        If ($LASTSUCCESS -eq $false) {
+            $LastSuccessColour = "Red"
+        }
+        Else {
+            $LastSuccessColour = "Cyan"
+        }
+        Write-Host ">$PWD>" -ForegroundColor Black -BackgroundColor $LastSuccessColour -NoNewline
+        Return " "
+    }
+}
+#EndRegion ---- Set Custom Prompt ----
+
 #Region ---- Configure PSDrives ----
 $LibPath = (Split-Path $PSScriptRoot -Parent)
 New-PSDrive -Name LibPath -PSProvider FileSystem -Root $LibPath | Out-Null
 #EndRegion ---- Configure PSDrives ----
 
 #Region ---- Import Custom Function Libraries ----
-$PublicFunctionFiles = [System.IO.Path]::Combine($LibPath,"Function Libraries","*.ps1")
+$PublicFunctionFiles = [System.IO.Path]::Combine($LibPath, "Function Libraries", "*.ps1")
 
 Get-ChildItem -Path $PublicFunctionFiles |
-    ForEach-Object {
-        Try {
-            . $_.FullName
-        } Catch {
-            Write-Warning "$($_.Exception.Message)"
-        }
+ForEach-Object {
+    Try {
+        . $_.FullName
     }
+    Catch {
+        Write-Warning "$($_.Exception.Message)"
+    }
+}
 #EndRegion ---- Import Custom Function Libraries ----
 
 #EndRegion Profile.ps1
